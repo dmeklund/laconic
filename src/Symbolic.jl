1cb3ccdcef3c8a6d5961572686dcfe402dac1372
David Eklund <deklund@gmail.com>
2019-06-30 17:51:12 -0400

    Add experimental symbolic support.

---
+++ src/Symbolic.jl
@@ -0,0 +1,57 @@
+module Symbolic
+    export AbstractStatement, AbstractExpression, Equals, BinaryAddition, NAryAddition
+    import Base
+
+    abstract type AbstractStatement{T <: Tuple} end
+    abstract type AbstractExpression{T <: Tuple} end
+    struct Equals{T1,T2} <: AbstractStatement{Tuple{T1,T2}}
+        lhs::T1
+        rhs::T2
+    end
+    # struct BinaryAddition{T1, T2} <: AbstractExpression{Tuple{T1,T2}}
+    #     first::T1
+    #     second::T2
+    # end
+    struct NAryAddition{T <: Tuple} <: AbstractExpression{T}
+        elements::T
+    end
+    NAryAddition(elements...) = NAryAddition(elements)
+    struct Division{T1, T2} <: AbstractExpression{Tuple{T1,T2}}
+        numerator::T1
+        denominator::T2
+    end
+    struct Product{T <: Tuple} <: AbstractExpression{T}
+        elements::T
+    end
+    Base.:*(first::T1, second::T2) where {T1 <: AbstractExpression, T2 <: AbstractExpression} = Product((first, second))
+    Base.:/(first::T1, second::T2) where {T1 <: AbstractExpression, T2 <: AbstractExpression} = Division(first, second)
+    Base.:+(first::NAryAddition, second::NAryAddition) = NAryAddition(first.elements..., second.elements...)
+    Base.:+(first::T1, second::T2) where {T1 <: AbstractExpression, T2 <: AbstractExpression} = NAryAddition((first, second))
+
+    struct Cosine{T} <: AbstractExpression{Tuple{T}}
+        argument::T
+    end
+    struct Sine{T} <: AbstractExpression{Tuple{T}}
+        argument::T
+    end
+    struct Exponential{T} <: AbstractExpression{Tuple{T}}
+        argument::T
+    end
+    struct Symbol <: AbstractExpression{Tuple{String}}
+        name::String
+    end
+    struct Numeric{T <: Number} <: AbstractExpression{Tuple{T}}
+        value::T
+    end
+    struct Vector{T <: AbstractArray} <: AbstractExpression{Tuple{T}}
+        value::T
+    end
+    function exercise2_1()
+        state_up = Vector([1, 0])
+        state_down = Vector([0, 1])
+        theta = Symbol("ϑ")
+        phi = Symbol("ϕ")
+        Cosine(theta / Numeric(2)) * state_up + Exponential(Numeric(im)*phi) * Sine(theta / Numeric(2)) * state_down
+
+    end
+end
