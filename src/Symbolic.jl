119fa8d15287340f8cb205b098baf48cc89214b8
David Eklund <deklund@gmail.com>
2019-07-22 21:31:39 -0400

    Improved symbolic support

--- src/Symbolic.jl
+++ src/Symbolic.jl
@@ -1,5 +1,4 @@
 module Symbolic
-    export AbstractStatement, AbstractExpression, Equals, BinaryAddition, NAryAddition
     using Laconic: Basis, MatrixType, is_orthonormal
     import LinearAlgebra
 
@@ -26,16 +25,31 @@ module Symbolic
         elements::T
     end
     Base.copy(expr::Product{T}) where T = Product{T}(expr.elements)
+    struct Power{T1 <: AbstractExpression, T2 <: AbstractExpression} <: AbstractExpression{Tuple{T1, T2}}
+        x::T1
+        y::T2
+    end
+    Product(x1::T1, x2::T2) where {T1,T2} = begin
+        if x1 == x2
+            Power{T1,Numeric}(x1, Numeric{Integer}(2))
+        else
+            Product{Tuple{T1,T2}}((x1,x2))
+        end
+    end
     struct Negation{T} <: AbstractExpression{Tuple{T}}
         element::T
     end
     struct Inversion{T} <: AbstractArray{AbstractExpression{Tuple{T}},2}
         element::Array{T,2}
     end
-    Base.:*(first::T1, second::T2) where {T1 <: AbstractExpression, T2 <: AbstractExpression} = Product((first, second))
+    function Base.:*(first::T1, second::T2) where {T1 <: AbstractExpression, T2 <: AbstractExpression}
+        Product(first, second)
+    end
     Base.:/(first::T1, second::T2) where {T1 <: AbstractExpression, T2 <: AbstractExpression} = Division(first, second)
     Base.:+(first::NAryAddition, second::NAryAddition) = NAryAddition(first.elements..., second.elements...)
-    Base.:+(first::T1, second::T2) where {T1 <: AbstractExpression, T2 <: AbstractExpression} = NAryAddition((first, second))
+    function Base.:+(first::T1, second::T2) where {T1 <: AbstractExpression, T2 <: AbstractExpression}
+        NAryAddition(first, second)
+    end
     Base.:-(first::T1, second::T2) where {T1 <: AbstractExpression, T2 <: AbstractExpression} = T1 + -T2
     Base.:-(object::T) where {T <: AbstractExpression} = Negation(object)
     LinearAlgebra.inv(expr::Array{T,2}) where {T <: AbstractExpression} = Inversion{T}(expr)
@@ -57,6 +71,9 @@ module Symbolic
     struct Numeric{T <: Number} <: AbstractExpression{Tuple{T}}
         value::T
     end
+    Base.:(==)(first::Numeric, second::Numeric) = (first.value == second.value)
+    Base.:(==)(first::Numeric, second::Number) = (first.value == second)
+    Base.:(==)(first::Number, second::Numeric) = (first == second.value)
     Base.one(elem::AbstractExpression) = Numeric{Integer}(1)
     struct Vector{T <: AbstractArray} <: AbstractExpression{Tuple{T}}
         value::T
@@ -79,9 +96,24 @@ module Symbolic
             xform |> MatrixType{AbstractExpression}
         )
         # @assert(is_orthonormal(xform))
-        print(xform)
-
+        # print(xform)
+        xform
         # Cosine(theta / Numeric(2)) * state_up + Exponential(Numeric(im)*phi) * Sine(theta / Numeric(2)) * state_down
 
     end
+
+    # simplification rules
+    function NAryAddition(val1::Power{Sine{T},Numeric}, val2::Power{Cosine{T},Numeric}) where T
+        if val1.y == 2 && val2.y == 2 && val1.x.argument == val2.x.argument
+            Numeric{Integer}(1)
+        else
+            NAryAddition((val1, val2))
+        end
+    end
+
+    export AbstractStatement, AbstractExpression
+    export Equals, BinaryAddition, NAryAddition
+    export Numeric, Symbol
+    export Product
+    export Sine, Cosine
 end
