15affbafb8945c68eba90ac3d22cad0b0778e4af
David Eklund <deklund@gmail.com>
2020-01-01 19:03:03 -0500

    Update system.jl for IntegrationVariable->Variable, add solver

--- src/system.jl
+++ src/system.jl
@@ -1,5 +1,6 @@
 module SystemM
     using Laconic.Calculus
+    using DifferentialEquations
     abstract type System end
 #     abstract type ElectromagneticSystem <: System end
 #     struct TimeIndependentMagneticField <: ElectromagneticSystem
@@ -16,16 +17,16 @@ module SystemM
 
     function propagator_element(hamiltonian, index, t0, t)
         if index == 1
-            t1 = IntegrationVariable("t1")
+            t1 = Variable("t1")
             return -im/hbar * DefiniteIntegral(t1, t0, t, hamiltonian(t1))
         elseif index == 2
-            t1 = IntegrationVariable("t1")
-            t2 = IntegrationVariable("t2")
+            t1 = Variable("t1")
+            t2 = Variable("t2")
             return -1/(2*hbar^2) * DefiniteIntegral(t1, t0, t, DefiniteIntegral(t2, t0, t1, commutator(hamiltonian(t1), hamiltonian(t2))))
         elseif index == 3
-            t1 = IntegrationVariable("t1")
-            t2 = IntegrationVariable("t2")
-            t3 = IntegrationVariable("t3")
+            t1 = Variable("t1")
+            t2 = Variable("t2")
+            t3 = Variable("t3")
             return im/(6*hbar^3) * DefiniteIntegral(t1, t0, t,
                 DefiniteIntegral(t2, t0, t1,
                     DefiniteIntegral(t3, t0, t2, (commutator(hamiltonian(t1), commutator(hamiltonian(t2), hamiltonian(t3))))))
@@ -33,5 +34,13 @@ module SystemM
         else
             error("Only up to three Magnus expansion elements supported")
         end
+    end
+
+    function solve_system(hamiltonian, basis, psi0, tspan)
+        function func!(dpsi, psi, p, t)
+            dpsi = 1/(im*hbar) * (hamiltonian * psi)
+        end
+        prob = ODEProblem(func!, psi0, tspan)
+        sol = solve(prob)
     end
 end
