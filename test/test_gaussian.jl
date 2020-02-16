ac282aafcb1e8a1a1e73d073da5d091b8ed8c30a
David Eklund <deklund@gmail.com>
2020-02-16 17:10:53 -0500

    Get initial Gaussian tests passing

--- test/test_gaussian.jl
+++ test/test_gaussian.jl
@@ -2,22 +2,26 @@ using Laconic.Gaussian
 
 function test_pgbf()
     s = PrimitiveGaussianBasisFunction(1.0)
-    px = PrimitiveGaussianBasisFunction(1.0, (0,0,0), (1.,0.,0.))
+    px = PrimitiveGaussianBasisFunction(1.0, (1,0,0), (0.,0.,0.))
     amp1 = amplitude(s, (0.,0.,0.))
     amp2 = amplitude(px, (0.,0.,0.))
-    print("Amplitude1 ($(amp1)) should be .71270547")
-    print("Amplitude2 ($(amp2)) should be 0")
-    # @assert isapprox(amplitude(s, (0.,0.,0.)), 0.71270547)
-    # @assert isapprox(amplitude(px, (0.,0.,0.)), 0)
+    # print("Amplitude1 ($(amp1)) should be .71270547")
+    # print("Amplitude2 ($(amp2)) should be 0")
+    @assert isapprox(amplitude(s, (0.,0.,0.)), 0.71270547)
+    @assert isapprox(amplitude(px, (0.,0.,0.)), 0)
 end
 test_pgbf()
 
-# function test_cgbf()
-#     c = cgbf(0.0,0.0,0.0)
-#     push!(c,1,1)
-#     @assert isapprox(amplitude(c,0,0,0),0.71270547)
-#     c2 = cgbf(0,0,0)
-#     push!(c2,1,0.2)
-#     push!(c2,0.5,0.2)
-#     @assert isapprox(overlap(c2,c2),1)
-# end
+function test_cgbf()
+    c = ContractedGaussianBasisFunction((0,0,0), (0.0, 0.0, 0.0), (1.0,1.0))
+    # push!(c,1,1)
+    @assert isapprox(amplitude(c, (0.,0.,0.)), 0.71270547)
+    c2 = ContractedGaussianBasisFunction(
+        (0,0,0),
+        (0., 0., 0.),
+        (1.0, 0.2),
+        (0.5, .2)
+    )
+    @assert isapprox(overlap(c2,c2),1)
+end
+test_cgbf()
